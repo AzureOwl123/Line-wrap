96aea74b9a6ec12602c22d13e04badfe4e142139
AzureOwl123 <95491246+AzureOwl123@users.noreply.github.com>
2021-12-08 17:38:21 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+/*
 function setup(){
   window.open(500,500);
 
@@ -8,3 +9,38 @@ function draw(){
   ellipse(40,40,40,40);
 
 }
+*/
+
+
+
+let star;
+
+
+function setup(){
+  window.open(500,500);
+  star = new Starship(50, 50);
+  
+  
+}
+
+
+function draw(){
+  star.display();
+  
+  
+}
+
+function Starship(width, height){
+  this.w = width;
+  this.h = height;
+  
+  
+  this.display = function(){
+    fill(0);
+    ellipse(this.w, this.h);
+    
+    
+    
+  };
+  
+  
